d43baa0f5303025e817549ea44b5672eba2be510
khaitbek <khaitbek2005@gmail.com>
2026-09-13 05:57:29 +0500

    fix: re-organize folders

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ config({ path: ['.env.local', '.env'], debug: true })
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  schema: './src/infra/adapters/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
--- src/integrations/better-auth/header-user.tsx
+++ src/integrations/better-auth/header-user.tsx
@@ -1,4 +1,4 @@
-import { authClient } from '#/lib/auth-client'
+import { authClient } from '#/infra/adapters/better-auth/auth-client.ts'
 
 export default function BetterAuthHeader() {
   const { data: session, isPending } = authClient.useSession()
